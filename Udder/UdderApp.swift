70f1d3684ec1ea8495c4406247a2cd1971919a24
Paul Hanson <paul@mangosoup.com>
2023-12-03 18:57:42 -0800

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Paul Hanson on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
